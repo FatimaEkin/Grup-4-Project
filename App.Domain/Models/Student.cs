81ed28572631dfbc7840bd83512c46fe8db0302d
Fatima Ekin <111661799+FatimaEkin@users.noreply.github.com>
2024-05-28 10:38:04 +0300

    [Feat] Student Class added

---
+++ App.Domain/Models/Student.cs
@@ -0,0 +1,8 @@
+﻿namespace App.Domain;
+
+public class Student
+{
+  public string Name { get; set; }
+  public string Surname { get; set; }
+  public int StudentNumber { get; set; }
+}
---
+++ App.Domain/Models/StudentList.cs
@@ -0,0 +1,24 @@
+﻿namespace App.Domain;
+
+public class StudentList
+{
+  private List<Student> students = new List<Student>();
+
+        public void AddStudent(Student student)
+        {
+            students.Add(student);
+        }
+
+        public void RemoveStudent(Student student)
+        {
+            students.Remove(student);
+        }
+
+        public void DisplayAllStudents()
+        {
+            foreach (var student in students)
+            {
+                Console.WriteLine($"Name: {student.Name}, Surname: {student.Surname}, Student Number: {student.StudentNumber}");
+            }
+        }
+}
